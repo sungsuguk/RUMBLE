17faa438b0da4ba9b1f8fa8e040cd8f0c61bd89f
sungsuguk <sungsuguk@gmail.com>
2011-08-14 16:51:37 +0800

    delete directory whose name is Chinese

--- ����ң��ͨ��ϵͳ/RX_Code/AT24C02Driver.h
+++
@@ -1,7 +0,0 @@
-//sbit SCL = P1^0;
-//sbit SDA = P1^1;
-
-
-
-extern unsigned char at24c02byteread(unsigned char address);
-extern void at24c02bytewrite(unsigned char address,unsigned char writedata);
